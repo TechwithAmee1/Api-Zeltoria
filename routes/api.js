9edf67a83cacdfd6b640d9f2f7511251a26ffca6
៚ᴍʀ.ᴀᴍɪʏᴀ༒ᴏꜰᴄ͢⁸⁵⁴¹༆࿐ <techwithamee@gmail.com>
2024-05-06 02:26:37 +0530

    Add files via upload

--- routes/api.js
+++ routes/api.js
@@ -316,8 +316,7 @@ router.get('/download/youtube', async (req, res, next) => {
 
     let hasil = await YoutubeSl(url, aqua)
     res.json(PromiseRes(hasil),
-    req.query.url,
-    req.query.quality).status(200)
+    res: req.query.url + req.query.quality).status(200)
   } catch (err) {
     res.status(500).json({ developer: develop, mess: `${err.toString()}. Report this error to Developer(Xorizn)` })
   }
